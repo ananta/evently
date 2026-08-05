efef5d96a39b73a279f66a7d5d2bafc396a430ad
ananta <12180395+ananta@users.noreply.github.com>
2026-08-05 09:59:01 -0500

    feat(api/json): add a 1MB limitation on json body and disallow multiple json objects in a request

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+  "strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -35,11 +36,18 @@ func (app *application) writeJson(w http.ResponseWriter, status int, data envelo
 
 // decode the request body and map it to dest
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
-  err := json.NewDecoder(r.Body).Decode(dst)
+  // limit the size of the request body to 1MB(1,048,576 bytes)
+  r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+  // disallow unknown fields before decoding
+  dec := json.NewDecoder(r.Body)
+  dec.DisallowUnknownFields()
+
+  err := dec.Decode(dst)
   if err != nil {
     var syntaxError *json.SyntaxError
     var unmarshalTypeError *json.UnmarshalTypeError
     var invalidUnmarshalError *json.InvalidUnmarshalError
+    var maxBytesError *http.MaxBytesError
 
     switch {
     case errors.As(err, &syntaxError):
@@ -53,11 +61,24 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
       return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
     case errors.Is(err, io.EOF):
       return errors.New("body must not be empty")
+    case strings.HasPrefix(err.Error(), "json: unknown field "):
+      fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+      return fmt.Errorf("body contains unknown key %s", fieldName)
+    case errors.As(err, &maxBytesError):
+      return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
     case errors.As(err, &invalidUnmarshalError):
       panic(err)
     default:
     return err
     }
+  }
+
+
+  // call the decode again, if its a success, it means we have multiple json objects, reject
+  err = dec.Decode(&struct{}{})
+  if !errors.Is(err, io.EOF){
+    return errors.New("body must only contain a single JSON value")
   }
   return nil
 }
