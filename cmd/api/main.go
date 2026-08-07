81e523a4c3866fcd35c17d5b58b3b92815420bad
ananta <12180395+ananta@users.noreply.github.com>
2026-08-07 02:23:49 -0500

    feat(api/db): open db pool on main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/ananta/evently/internal/data"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,11 +20,18 @@ const version = "1.0.0"
 type config struct {
   port int
   env string
+  db struct {
+    dsn string
+    maxOpenConns int
+    maxIdleConns int
+    maxIdleTime time.Duration
+  }
 }
 
 type application struct {
   config config
   logger *slog.Logger
+  models data.Models
 }
 
 func main() {
@@ -27,9 +40,23 @@ func main() {
 
   flag.IntVar(&cfg.port, "port", 4000, "API server port")
   flag.StringVar(&cfg.env, "env", "dev", "Application environment")
+  flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://evently_user:evently_password@localhost/evently_db?sslmode=disable", "PostgreSQL DSN")
+  flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns" , 25, "PostgreSQL max open connections")
+  flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns" , 25, "PostgreSQL max idle connections")
+  flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time" , 15*time.Minute, "PostgreSQL max connection idle time")
   flag.Parse()
 
   logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+  db, err := openDB(cfg)
+  if err!= nil {
+    logger.Error(err.Error())
+    os.Exit(1)
+  }
+  defer db.Close()
+
+  logger.Info("database connection pool established")
+
 
   app := application {
     config: cfg,
@@ -48,9 +75,33 @@ func main() {
 
   logger.Info("starting server", "port", srv.Addr)
 
-  err := srv.ListenAndServe()
+  // TODO: should this be a non-blocking?
+  app.seedOperationTypes()
+
+  err = srv.ListenAndServe()
   logger.Error(err.Error())
   os.Exit(1)
+}
 
 
+func openDB(cfg config)(*sql.DB, error){
+  db, err := sql.Open("postgres", cfg.db.dsn)
+  if err != nil {
+    return nil, err
+  }
+  db.SetMaxOpenConns(cfg.db.maxOpenConns)
+  db.SetMaxIdleConns(int(cfg.db.maxIdleConns))
+  db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+  // create a context with a 5 sec timeout deadline
+  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+  defer cancel()
+
+  err = db.PingContext(ctx)
+  if err != nil {
+    db.Close()
+    return nil, err
+  }
+
+  return db, nil
 }
