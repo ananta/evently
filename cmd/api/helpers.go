064e62920b81f4da61d2a4c7cccba86372bf6ab6
ananta <12180395+ananta@users.noreply.github.com>
2026-08-07 02:40:54 -0500

    feat(api/helper): add helper method to read id param from req param

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -13,6 +13,18 @@ import (
 
 )
 
+// Retreive the "id" URL parameter from current request context
+// convert it to an integer and return it
+// if the operation isn't successful, return 0 and an error
+func (app *application) readIDParam(r *http.Request)(int, error) {
+  params := httprouter.ParamsFromContext(r.Context())
+  id, err := strconv.Atoi(params.ByName("id"))
+  if err != nil || id < 1 {
+    return 0, errors.New("invalid id parameter")
+  }
+  return id, nil
+}
+
 type envelope map[string]any
 // write json, we could use json.NewEncoder(), but this allows us to set conditional http response headers
 // also minimal perf diff compared to json.NewEncoder()
