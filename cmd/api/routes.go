e698c2e14588cd1589ad916cc1104bb099b7b730
ananta <12180395+ananta@users.noreply.github.com>
2026-08-07 09:52:50 -0500

    refactor(api): unversion health route for simplicity

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/healthcheck", app.healthcheckHandler)
 	// retrieve the account information
 	router.HandlerFunc(http.MethodGet, "/accounts/:id", app.getAccount)
 	// create an account
