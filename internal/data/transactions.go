100f3a7e8e8a9fbf42e57c24f098d5d2d84a0900
ananta <12180395+ananta@users.noreply.github.com>
2026-08-06 21:50:13 -0500

    fix(api/models): resolve transaction model conflicts

--- internal/data/transactions.go
+++ internal/data/transactions.go
@@ -1,9 +1,10 @@
 package data
 
-type Transactions struct {
-  Transaction_ID int `json:"transaction_id"`
-  Account_ID int `json:"account_id"`
-  OperationType_ID int  `json:"operation_type_id"`
-  Amount Amount  `json:"amount,omitzero"`
+
+type Transaction struct {
+  Transaction_ID int64 `json:"transaction_id"`
+  Account_ID int64 `json:"account_id"`
+  OperationType_ID int64  `json:"operation_type_id"`
+  Amount float32  `json:"amount,omitzero"`
   EventDate string  `json:"event_date"`
 }
